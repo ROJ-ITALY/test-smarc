4aa48adfe1f100157818a51b6cae8b6dcf9d3a4d
Stefano Gurrieri <stefano.gurrieri@roj.com>
2017-03-06 12:02:11 +0100

    - Fix test_can.sh fails

--- test_can.sh
+++ test_can.sh
@@ -57,16 +57,18 @@ ERROR_RESPONSE_FROM_CAN0=2
 ip link set can0 up type can bitrate 125000 2> /dev/null
 ip link set can1 up type can bitrate 125000 2> /dev/null
 
+sleep 1
+
 candump can0 > /tmp/cantest.txt &
+sleep 1
 cansend can1 -i 0x5a1 0x0a 0x0b 0x0c
 
 RESULT=$(cat /tmp/cantest.txt | tail -n 1)
 
-sleep 1
-
 if [ "$RESULT" == "<0x5a1> [3] 0a 0b 0c " ]
 then
 	candump can1 > /tmp/cantest.txt &
+	sleep 1
 	cansend can0 -i 0x5a1 0x0a 0x0b 0x0c
 		
 	RESULT=$(cat /tmp/cantest.txt | tail -n 1)
